96090d6a55976dd128c7a46f5bb4d605899fad60
Satvik Bhatnagar <satvikbhatnagar@gmail.com>
2022-08-23 11:56:31 +0530

    Data pre-processing template created

---
+++ 01_ Data Preprocessing-template.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 23 11:36:10 2022
+
+@author: satvik
+"""
+
+'''DATA PRE-PROCESSING TOOLS'''
+
+#-------Importing the libraries
+
+
+#-------Importing the dataset
+
+
+#-------Taking care of missing data
+
+
+#-------Encoding categorical data
+
+
+#-------Encoding the Independent Variable
+
+
+#-------Encoding the Dependent Variable
+
+
+#-------Splitting the dataset into the Training set and Test set
+
+
+#-------Feature Scaling
