624b244e3821df4c8c0530c0f91b8010d153f69c
Satvik Bhatnagar <satvikbhatnagar@gmail.com>
2022-08-23 11:57:15 +0530

    importing libraries

---
+++ 01_ Data Preprocessing.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 23 11:36:10 2022
+
+@author: satvik
+"""
+
+'''DATA PRE-PROCESSING TOOLS'''
+
+#-------Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+#-------Importing the dataset
+
+
+#-------Taking care of missing data
+
+
+#-------Encoding categorical data
+
+
+#-------Encoding the Independent Variable
+
+
+#-------Encoding the Dependent Variable
+
+
+#-------Splitting the dataset into the Training set and Test set
+
+
+#-------Feature Scaling
