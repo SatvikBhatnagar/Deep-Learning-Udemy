73e8b32081679ad6f5d52cd35d1f3a15b4c7e28e
Satvik Bhatnagar <satvikbhatnagar@gmail.com>
2022-08-23 12:04:08 +0530

    handling missing data

--- 01_ Data Preprocessing.py
+++ 01_ Data Preprocessing.py
@@ -22,6 +22,10 @@ y = dataset.iloc[:, -1].values
 
 
 #-------Taking care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 
 #-------Encoding categorical data
