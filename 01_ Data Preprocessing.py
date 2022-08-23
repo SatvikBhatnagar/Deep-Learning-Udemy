f81367940f280f26b23baee892d6a87d613ebd55
Satvik Bhatnagar <satvikbhatnagar@gmail.com>
2022-08-23 19:30:45 +0530

    feature scaling

--- 01_ Data Preprocessing.py
+++ 01_ Data Preprocessing.py
@@ -50,3 +50,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 
 
 #-------Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = sc.transform(X_test[:, 3:])
