debaed6dc8a77fef39681baf9ca1304b83f97af6
Satvik Bhatnagar <satvikbhatnagar@gmail.com>
2022-08-25 01:15:55 +0530

    predicting the test set results

--- 02_logistic_regression.py
+++ 02_logistic_regression.py
@@ -37,7 +37,8 @@ classifier.fit(X_train, y_train)
 print(classifier.predict(sc.transform([[30, 87000]])))
 
 #Predicting the Test set results
-
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
 
 #Making the Confusion Matrix
 
