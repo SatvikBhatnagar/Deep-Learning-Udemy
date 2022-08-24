189eb79ad0d851ba5e45ef4cc9bf9988901ae165
Satvik Bhatnagar <satvikbhatnagar@gmail.com>
2022-08-25 00:39:03 +0530

    Pre-processing data

--- 02_logistic_regression.py
+++ 02_logistic_regression.py
@@ -8,13 +8,19 @@ Created on Thu Aug 25 00:18:04 2022
 """Logistic Regression"""
 
 #Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 #Importing the dataset
-
+dataset = pd.read_csv("Materials/Part 3 - Classification/Section 14 - Logistic Regression/Python/Social_Network_Ads.csv")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
 
 #Splitting the dataset into the Training set and Test set
-
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 #Feature Scaling
 
