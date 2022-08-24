4b62cf451226926fb448ce7867974b2101a038f0
Satvik Bhatnagar <satvikbhatnagar@gmail.com>
2022-08-25 01:50:14 +0530

    Making the Confusion Matrix

--- 02_logistic_regression.py
+++ 02_logistic_regression.py
@@ -34,14 +34,17 @@ classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
 
 #Predicting a new result
-print(classifier.predict(sc.transform([[30, 87000]])))
+classifier.predict(sc.transform([[30, 87000]]))
 
 #Predicting the Test set results
 y_pred = classifier.predict(X_test)
-print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1)
 
 #Making the Confusion Matrix
-
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+accuracy_score(y_test, y_pred)
 
 #Visualising the Training set results
 
