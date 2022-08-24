6d3b9d187210adf1b0b659b616c5fa1e7d15df2d
Satvik Bhatnagar <satvikbhatnagar@gmail.com>
2022-08-25 00:22:56 +0530

    Create 02_logistic_regression.py

---
+++ 02_logistic_regression.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 25 00:18:04 2022
+
+@author: satvik
+"""
+
+"""Logistic Regression"""
+
+#Importing the libraries
+
+
+#Importing the dataset
+
+
+#Splitting the dataset into the Training set and Test set
+
+
+#Feature Scaling
+
+
+#Training the Logistic Regression model on the Training set
+
+
+#Predicting a new result
+
+
+#Predicting the Test set results
+
+
+#Making the Confusion Matrix
+
+
+#Visualising the Training set results
+
+
+#Visualising the Test set results
