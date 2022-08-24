3fef61ca97e757aa94dbefa211138da6055762e9
Satvik Bhatnagar <satvikbhatnagar@gmail.com>
2022-08-25 01:07:27 +0530

    predicting a new result

--- 02_logistic_regression.py
+++ 02_logistic_regression.py
@@ -34,7 +34,7 @@ classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
 
 #Predicting a new result
-
+print(classifier.predict(sc.transform([[30, 87000]])))
 
 #Predicting the Test set results
 
